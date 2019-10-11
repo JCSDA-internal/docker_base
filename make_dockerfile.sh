c87a2a7f5c48a89b1a2d0fa1f1587bd42101c4c1
Mark Miesch <miesch@ucar.edu>
2019-10-10 21:24:36 -0600

    Added clang-mpich container

--- make_dockerfile.sh
+++ make_dockerfile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [[ $# -ne 2 ]]; then
+   echo "usage: make_dockerfile <compiler> <mpi>"
+   exit 1
+fi
+
 HPCCM=../hpc-container-maker/hpccm.py
 
-$HPCCM --recipe base.py --format docker > Dockerfile
+$HPCCM --recipe base_$1_$2.py --format docker > Dockerfile.$1_$2
 echo "Generated with hpccm version: " > generated.version
 $HPCCM --version >> generated.version
