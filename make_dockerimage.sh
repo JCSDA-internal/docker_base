c24b19b6a60f5b4ef5679cbe9109dc418a078be0
Mark J Olah <markjolah@users.noreply.github.com>
2019-11-19 12:20:33 -0700

    Update make_dockerfile.sh to accept location of hpccm executable as optional argument (#22)

--- make_dockerimage.sh
+++ make_dockerimage.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-   echo "usage: make_dockerfile <name>"
+#
+# make_dockerfile.sh - Generate the dockerfile from base.py via HPCCM
+#
+# Useage:
+# make_dockerfile.sh <name> <hpccm>
+#
+# <name> - name of image to build in format <compiler>-<mpi>-<type>
+#           where <type> is dev (development) or app (application)
+#           Default: gnu-openmpi-dev
+#
+# <hpccm> - location of hpccm.py or hpccm executable
+#           Default: ../hpc-container-maker/hpccm.py
+#
+if [[ $# -lt 1 ]]; then
+   echo "usage: make_dockerfile <name> <hpccm>"
    exit 1
 fi
 
-HPCCM=../hpc-container-maker/hpccm.py
+NAME=${1:-"gnu-openmpi-dev"}
+HPCCM=${2:-../hpc-container-maker/hpccm.py}
 
-$HPCCM --recipe base-$1.py --format docker > Dockerfile.$1
+$HPCCM --recipe base-$NAME.py --format docker > Dockerfile.$NAME
 echo "Generated with hpccm version: " > generated.version
 $HPCCM --version >> generated.version
 
-docker image build --no-cache -f Dockerfile.$1 -t jcsda/docker_base-$1 .
+docker image build --no-cache -f Dockerfile.$NAME -t jcsda/docker_base-$NAME .
 
 # might want to do this manually after testing
-# docker push jcsda/docker_base-$1
+# docker push jcsda/docker_base-$NAME
