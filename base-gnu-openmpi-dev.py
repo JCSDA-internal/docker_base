a3f6b1a78d86c83a56f148fd3eb900710668938b
mmiesch <miesch@ucar.edu>
2020-04-07 12:16:00 -0600

    unify hpc and dev container with hpccm userargs

--- base-gnu-openmpi-dev.py
+++ base-gnu-openmpi-dev.py
@@ -1,15 +1,25 @@
-"""Prototype JEDI docker_base container
+"""Prototype JEDI docker_base development container for gnu-openmpi
 
 Usage:
-$ ../hpc-container-maker/hpccm.py --recipe base.py --format docker > Dockerfile
+hpccm --recipe base-gnu-openmpi-dev.py --userarg hpc="True" psm="True" --format docker > Dockerfile
 """
 
 # Base image
 Stage0.baseimage('ubuntu:18.04')
 
+# get optional user arguments
+hpcstring = USERARG.get('hpc', 'False')
+mxofed = USERARG.get('mellanox', 'False')
+psm = USERARG.get('psm', 'False')
+
+if (hpcstring.lower() == "true"):
+    hpc=True
+else:
+    hpc=False
+
 # update apt keys
-#Stage0 += shell(commands=['apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157',
-#                          'apt-get update'])
+Stage0 += shell(commands=['apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157',
+                          'apt-get update'])
 
 # useful system tools 
 # libexpat is required by udunits
@@ -24,7 +34,7 @@ Stage0 += apt_get(ospackages=['build-essential','tcsh','csh','ksh','apt-utils',
 Stage0 += gnu(version='7')
 
 # get an up-to-date version of CMake
-Stage0 += cmake(eula=True,version="3.13.0")
+Stage0 += cmake(eula=True,version="3.16.0")
 
 # editors, document tools, git, and git-flow                   
 Stage0 += apt_get(ospackages=['emacs','vim','nedit','graphviz','doxygen',
@@ -34,7 +44,7 @@ Stage0 += apt_get(ospackages=['emacs','vim','nedit','graphviz','doxygen',
 Stage0 += shell(commands=
                 ['curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash',
                  'apt-get update','apt-get install -y --no-install-recommends git-lfs', 'git lfs install'])
-                  
+
 # autoconfig and debuggers                  
 Stage0 += apt_get(ospackages=['autoconf','pkg-config','ddd','gdb','kdbg','valgrind'])
     
@@ -43,15 +53,36 @@ Stage0 += apt_get(ospackages=['python3-pip','python3-dev','python3-yaml',
                               'python3-scipy'])
 Stage0 += shell(commands=['ln -s /usr/bin/python3 /usr/bin/python'])
 
-# Mellanox OFED
-Stage0 += mlnx_ofed(version='4.5-1.0.1.0')
+# Mellanox or inbox OFED
+if (hpc):
+    if (mxofed.lower() == "true"): 
+        Stage0 += mlnx_ofed(version='4.5-1.0.1.0')
+    else:
+        Stage0 += ofed()
+    infiniband=True
 
-# PSM library
-Stage0 += apt_get(ospackages=['libpsm-infinipath1','libpsm-infinipath1-dev'])
+    # PSM library
+    if (psm.lower() == "true"):
+        Stage0 += apt_get(ospackages=['libpsm-infinipath1','libpsm-infinipath1-dev'])
+        withpsm=True
+    else:
+        withpsm=False
+
+    # PMI library
+    Stage0 += slurm_pmi2()
+
+    # UCX and components
+    Stage0 += knem()
+    Stage0 += xpmem()
+    Stage0 += ucx(ofed=True,knem=True,xpmem=True,cuda=False)
+
+else:
+    infiniband=False
 
 # OpenMPI
-Stage0 += openmpi(prefix='/usr/local', version='3.1.2', cuda=False, infiniband=True, 
-                  configure_opts=['--enable-mpi-cxx --with-psm'])
+Stage0 += openmpi(prefix='/usr/local', version='3.1.2', cuda=False, infiniband=infiniband, 
+                  pmi="/usr/local/slurm-pmi2",ucx="/usr/local/ucx", with_psm=withpsm,
+                  configure_opts=['--enable-mpi-cxx'])
 
 # locales time zone and language support
 Stage0 += shell(commands=['apt-get update',
