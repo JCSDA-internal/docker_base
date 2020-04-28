6df1f6828eed2e15002cb905ec57114e973c0875
Mark Miesch <miesch@ucar.edu>
2020-04-28 15:00:32 -0600

    Feature/hpc (#28)
    * hpc gnu-openmpi container recipe
    
    * modifications for hpc and update hpccm
    
    * gnu hpc container builds
    
    * add gnu time to base containers
    
    * unify hpc and dev container with hpccm userargs
    
    * unify hpc container build with standard dev container
    
    * unify gnu and clang builds into one hpccm file
    
    * enhance push option to build script
    
    * bugfix
    
    * bugfix
    
    * add htlatex and imagemagick for atlas doc generation

---
+++ base-dev.py
@@ -0,0 +1,126 @@
+"""JEDI docker_base development container
+
+Usage:
+hpccm --recipe base-dev.py --userarg compiler="gnu" mpi="openmpi" hpc="True" psm="True" --format docker > Dockerfile
+"""
+
+# Base image
+Stage0.baseimage('ubuntu:18.04')
+
+# get optional user arguments
+mycompiler = USERARG.get('compiler', 'gnu')
+mympi = USERARG.get('mpi', 'openmpi')
+hpcstring = USERARG.get('hpc', 'False')
+mxofed = USERARG.get('mellanox', 'False')
+psm = USERARG.get('psm', 'False')
+
+if (hpcstring.lower() == "true"):
+    hpc=True
+else:
+    hpc=False
+
+# update apt keys
+Stage0 += apt_get(ospackages=['build-essential','gnupg2','apt-utils'])
+Stage0 += shell(commands=['apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157',
+                          'apt-get update'])
+
+# useful system tools 
+# libexpat is required by udunits
+Stage0 += apt_get(ospackages=['tcsh','csh','ksh', 'openssh-server','libncurses-dev',
+                              'libssl-dev','libx11-dev','less','man-db','tk','tcl','swig',
+                              'bc','file','flex','bison','libexpat1-dev',
+                              'libxml2-dev','unzip','wish','curl','wget','time',
+                              'libcurl4-openssl-dev','nano','screen','lsb-release'])
+
+# Install GNU compilers - even clang needs gfortran
+Stage0 += gnu(version='7')
+
+# Install clang compilers 
+if (mycompiler.lower() == "clang"):
+    Stage0 += llvm(extra_repository=True, version='8')
+
+# get an up-to-date version of CMake
+Stage0 += cmake(eula=True,version="3.16.0")
+
+# editors, document tools, git, and git-flow                   
+Stage0 += apt_get(ospackages=['emacs','vim','nedit','graphviz','doxygen',
+                              'texlive-latex-recommended','texinfo','lynx',
+                              'git','git-flow','imagemagick','tex4ht'])
+# git-lfs
+Stage0 += shell(commands=
+                ['curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash',
+                 'apt-get update','apt-get install -y --no-install-recommends git-lfs', 'git lfs install'])
+
+# autoconfig and debuggers                  
+Stage0 += apt_get(ospackages=['autoconf','pkg-config','ddd','gdb','kdbg','valgrind'])
+    
+# python3
+Stage0 += apt_get(ospackages=['python3-pip','python3-dev','python3-yaml',
+                              'python3-scipy'])
+Stage0 += shell(commands=['ln -s /usr/bin/python3 /usr/bin/python'])
+
+# Mellanox or inbox OFED
+if (hpc):
+    if (mxofed.lower() == "true"): 
+        Stage0 += mlnx_ofed(version='4.5-1.0.1.0')
+    else:
+        Stage0 += ofed()
+    infiniband=True
+
+    # PSM library
+    if (psm.lower() == "true"):
+        Stage0 += apt_get(ospackages=['libpsm-infinipath1','libpsm-infinipath1-dev'])
+        withpsm=True
+    else:
+        withpsm=False
+
+    # PMI library
+    Stage0 += slurm_pmi2()
+
+    # UCX and components
+    Stage0 += knem()
+    Stage0 += xpmem()
+    Stage0 += ucx(ofed=True,knem=True,xpmem=True,cuda=False)
+
+    if (mympi.lower() == "mpich"):
+
+        #mpich
+        # have to do this manually since the builing block doesn't work
+        if (mycompiler.lower() == "clang"):
+            Stage0 += environment(variables={'FC':'gfortran','CC':'clang','CXX':'clang++',
+                'CFLAGS':'-fPIC','CXXFLAGS':'-fPIC','FCFLAGS':'-fPIC'})
+        else:
+            Stage0 += environment(variables={'FC':'gfortran','CC':'gcc','CXX':'g++',
+                'CFLAGS':'-fPIC','CXXFLAGS':'-fPIC','FCFLAGS':'-fPIC'})
+        Stage0 += mpich(version='3.3.1', configure_opts=['--enable-cxx --enable-fortran'])
+    
+    else:
+        # OpenMPI
+        Stage0 += openmpi(prefix='/usr/local', version='3.1.2', cuda=False, infiniband=infiniband, 
+                          pmi="/usr/local/slurm-pmi2",ucx="/usr/local/ucx", with_psm=withpsm,
+                          configure_opts=['--enable-mpi-cxx'])
+else:
+
+    if (mympi.lower() == "mpich"):
+
+        #mpich
+        Stage0 += environment(variables={'FC':'gfortran','CC':'clang','CXX':'clang++',
+            'CFLAGS':'-fPIC','CXXFLAGS':'-fPIC','FCFLAGS':'-fPIC'})
+        Stage0 += mpich(version='3.3.1', configure_opts=['--enable-cxx --enable-fortran'])
+    
+    else:
+        # OpenMPI
+        Stage0 += openmpi(prefix='/usr/local', version='3.1.2', cuda=False, infiniband=False, 
+                          configure_opts=['--enable-mpi-cxx'])
+
+# locales time zone and language support
+Stage0 += shell(commands=['apt-get update',
+     'DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata locales',
+     'ln -fs /usr/share/zoneinfo/America/Denver /etc/localtime',
+     'locale-gen --purge en_US.UTF-8',
+     'dpkg-reconfigure --frontend noninteractive tzdata',
+     'dpkg-reconfigure --frontend=noninteractive locales',
+     'update-locale \"LANG=en_US.UTF-8\"',
+     'update-locale \"LANGUAGE=en_US:en\"'])
+Stage0 += environment(variables={'LANG':'en_US.UTF-8','LANGUAGE':'en_US:en'})
+
--- make_dockerimage.sh
+++ make_dockerimage.sh
@@ -4,7 +4,7 @@
 # make_dockerfile.sh - Generate the dockerfile from base.py via HPCCM
 #
 # Useage:
-# make_dockerfile.sh <name> <tag> <hpccm>
+# make_dockerfile.sh <name> <tag> <hpc>
 #
 # <name> - name of image to build in format <compiler>-<mpi>-<type>
 #           where <type> is dev (development) or app (application)
@@ -12,23 +12,103 @@
 #
 # <tag> - optional tag for docker image
 #
-# <hpccm> - location of hpccm.py or hpccm executable
-#           Default: ../hpc-container-maker/hpccm.py
+# <hpc> - this selects amoung possible hpc configurations
+#         accepted values include:
+#         0 (default): No hpc options
+#         1: base configuration - sufficient for, e.g., S4
+#            - UCX with KNEM, XPMEM
+#            - OFED: linux inbox
+#            - PMI: slurm-pmi2
+#            - PSM: None
+#         2: with Mellanox OFED and PSM
+#            - UCX with KNEM, XPMEM
+#            - OFED: Mellanox
+#            - PMI: slurm-pmi2
+#            - PSM: Yes
 #
+#------------------------------------------------------------------------
+function get_ans {
+    ans=''
+    while [[ $ans != y ]] && [[ $ans != n ]]; do
+      echo $1
+      read ans < /dev/stdin
+      if [[ $ans != y ]] && [[ $ans != n ]]; then echo "You must enter y or n"; fi
+    done
+}
+
+#------------------------------------------------------------------------
 if [[ $# -lt 1 ]]; then
-   echo "usage: make_dockerfile <name> <hpccm>"
+   echo "usage: make_dockerfile <name>"
    exit 1
 fi
 
-NAME=${1:-"gnu-openmpi-dev"}
+CNAME=${1:-"gnu-openmpi-dev"}
 TAG=${2:-"latest"}
-HPCCM=${3:-../hpc-container-maker/hpccm.py}
+HPC=${3:-"0"}
 
-$HPCCM --recipe base-$NAME.py --format docker > Dockerfile.$NAME
+CompilerName=$(echo $CNAME| cut -d- -f1)
+MPIName=$(echo $CNAME| cut -d- -f2)
+
+case ${HPC} in
+    "0")
+        hpccm --recipe base-dev.py --userarg compiler=${CompilerName} \
+                                                     mpi=${MPIName} \
+	                                              --format docker > Dockerfile.$CNAME
+        ;;
+    "1")
+        hpccm --recipe base-dev.py --userarg compiler=${CompilerName} \
+                                                     mpi=${MPIName} \
+                                                     hpc="True" \
+                                                      --format docker > Dockerfile.$CNAME
+        ;;
+    "2")
+        hpccm --recipe base-dev.py --userarg compiler=${CompilerName} \
+                                                     mpi=${MPIName} \
+                                                     hpc="True" \
+                                                     mellanox="True" \
+                                                     psm="True" \
+                                                     --format docker > Dockerfile.$CNAME
+        ;;
+    *)
+        echo "ERROR: unsupported HPC option"
+	exit 1
+        ;;
+esac
+
 echo "Generated with hpccm version: " > generated.version
-$HPCCM --version >> generated.version
+hpccm --version >> generated.version
 
-docker image build --no-cache -f Dockerfile.$NAME -t jcsda/docker_base-$NAME:$TAG .
+if [[ ${TAG} == 'latest' ]]; then
 
-# might want to do this manually after testing
-# docker push jcsda/docker_base-$NAME
+    docker image build --no-cache -f Dockerfile.$CNAME -t jcsda/docker_base-$CNAME:beta .
+
+    #------------------------------------------------------------------------
+    get_ans "Push to Docker Hub and back up?"
+
+    if [[ $ans == y ]] ; then
+
+        # save previous image in case something goes wrong
+        docker pull jcsda/docker_base-$CNAME:latest
+        docker tag jcsda/docker_base-$CNAME:latest jcsda/docker_base-$CNAME:revert
+        docker push jcsda/docker_base-$CNAME:revert
+        docker rmi jcsda/docker_base-$CNAME:latest
+
+        # push new image and re-tag it with latest
+        docker tag jcsda/docker_base-$CNAME:beta jcsda/docker_base-$CNAME:latest
+        docker rmi jcsda/docker_base-$CNAME:beta
+        docker push jcsda/docker_base-$CNAME:latest
+
+    fi
+    #------------------------------------------------------------------------
+
+else
+
+    docker image build --no-cache -f Dockerfile.$CNAME -t jcsda/docker_base-$CNAME:${TAG} .
+
+    get_ans "Push to Docker Hub?"
+
+    if [[ $ans == y ]] ; then
+        docker push jcsda/docker_base-$CNAME:${TAG}
+    fi
+
+fi
